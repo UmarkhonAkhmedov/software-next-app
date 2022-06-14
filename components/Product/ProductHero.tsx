10efdf0f7b9397cb5f65ec3dc4ebea4932d548f1
Umarkhon <umarkhonakhmedov2000@gmail.com>
2022-06-14 15:19:41 +0500

    Creating Hero Section for Product Page

---
+++ components/Product/ProductHero.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function ProductHero() {
+  return (
+    <div className='ProHero'>
+      <div className='container'>
+        <div className='proHero'>
+          <div className='proHero__heading'>
+            <h1>Grow your Sales and Services</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className='proHero__heading--buttons'>
+              <button className="btn__dark">
+                <Link href="/" passHref>Get Started</Link>
+              </button>
+              <button className="btn__dark">
+                <Link href="/" passHref>Get Started</Link>
+              </button>
+            </div>
+          </div>
+          <div className='proHero__img'>
+            <Image src="/Img/Product/hero__dashboard.png" width={550} height={360} priority objectFit='contain' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductHero
--- pages/Product.tsx
+++ pages/Product.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Brands from '../components/Brands'
+import ProductHero from '../components/Product/ProductHero'
 
 function Product() {
   return (
     <div>
-      <Brands />
+      <div className='product__hero--background'>
+        <ProductHero />
+        <Brands />
+      </div>
     </div>
   )
 }
