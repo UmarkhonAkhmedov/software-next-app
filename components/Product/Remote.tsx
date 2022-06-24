7d635b9fe9b68a271ec217c670877018aa3e3d9e
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-06-24 19:44:17 +0500

    Adding Remote component for Product Pahe

---
+++ components/Product/Remote.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Remote() {
+  return (
+    <div className='marketing tracking'>
+      <div className='marketing__img'>
+        <Image src="/Img/Product/tracking__main-img.svg" width={576} height={434} objectFit="contain" alt="" />
+      </div>
+      <div className='marketing__info'>
+        <h4 className='title__small--blur'>Remote teams</h4>
+        <h2>Real-time collaboration</h2>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Remote
--- pages/Product.tsx
+++ pages/Product.tsx
@@ -4,6 +4,7 @@ import Features from '../components/Features'
 import Generation from '../components/Product/Generation'
 import Marketing from '../components/Product/Marketing'
 import ProductHero from '../components/Product/ProductHero'
+import Remote from '../components/Product/Remote'
 import Service from '../components/Product/Service'
 import Tracker from '../components/Product/Tracker'
 
@@ -15,14 +16,15 @@ function Product() {
         <Brands />
       </div>
       <Service />
-      <Features />
       <div className="product__info">
         <div className='container'>
           <Marketing />
           <Tracker />
           <Generation />
+          <Remote />
         </div>
       </div>
+      <Features />
     </div>
   )
 }
