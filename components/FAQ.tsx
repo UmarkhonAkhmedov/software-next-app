9d28b4ee7c140e79a5e1dc23e0b5caa4ff899c17
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-01 05:45:56 +0500

    Comment

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,39 +1,39 @@
 import Link from 'next/link';
-import React from 'react';
-import { Accordion, AccordionItem } from 'react-light-accordion';
-import { accordionData } from './DummyData';
+// import React from 'react';
+// import { Accordion, AccordionItem } from 'react-light-accordion';
+// import { accordionData } from './DummyData';
 
 
-function FAQ() {
-  return (
-    <div className='FAQ'>
-      <div className='container'>
-        <div className='faq'>
-          <div className='faq__heading'>
-            <h2>Frequently asked questions</h2>
-            <p className='FAQ__heading--text'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
-          </div>
-          <div className='faq__accordion'>
-            <Accordion atomic={true} className="accordion">
-              <AccordionItem className="accordion__title" title={accordionData[0].title}>
-                <p className='faq__accordion--text'>{accordionData[0].text}</p>
-              </AccordionItem>
-              <AccordionItem className="accordion__title" title={accordionData[1].title}>
-                <p className='faq__accordion--text'>{accordionData[0].text}</p>
-              </AccordionItem>
-              <AccordionItem className="accordion__title" title={accordionData[2].title}>
-                <p className='faq__accordion--text'>{accordionData[0].text}</p>
-              </AccordionItem>
-              <AccordionItem className="accordion__title" title={accordionData[3].title}>
-                <p className='faq__accordion--text'>{accordionData[0].text}</p>
-              </AccordionItem>
-            </Accordion>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-};
+// function FAQ() {
+//   return (
+//     <div className='FAQ'>
+//       <div className='container'>
+//         <div className='faq'>
+//           <div className='faq__heading'>
+//             <h2>Frequently asked questions</h2>
+//             <p className='FAQ__heading--text'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
+//           </div>
+//           <div className='faq__accordion'>
+//             <Accordion atomic={true} className="accordion">
+//               <AccordionItem className="accordion__title" title={accordionData[0].title}>
+//                 <p className='faq__accordion--text'>{accordionData[0].text}</p>
+//               </AccordionItem>
+//               <AccordionItem className="accordion__title" title={accordionData[1].title}>
+//                 <p className='faq__accordion--text'>{accordionData[0].text}</p>
+//               </AccordionItem>
+//               <AccordionItem className="accordion__title" title={accordionData[2].title}>
+//                 <p className='faq__accordion--text'>{accordionData[0].text}</p>
+//               </AccordionItem>
+//               <AccordionItem className="accordion__title" title={accordionData[3].title}>
+//                 <p className='faq__accordion--text'>{accordionData[0].text}</p>
+//               </AccordionItem>
+//             </Accordion>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// };
 
 
-export default FAQ;
+// export default FAQ;
